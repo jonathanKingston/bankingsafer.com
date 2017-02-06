1db217dbb8927e76030a625217302afb42652e67
Jonathan Kingston <jonathan@jooped.co.uk>
2017-02-06 14:22:42 +0000

    Adding in world banks

--- js/httpobs-dashboard.js
+++ js/httpobs-dashboard.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     'use strict';
 
     // initialize all the popovers
-    $(function () { $('.grade[data-toggle="popover"]').popover(
+    $(function () { $('.grade[data-toggle="popover"], a[data-toggle="popover"]').popover(
         {
             html: true,
             placement: 'top',
@@ -13,26 +13,31 @@ $(document).ready(function() {
     // the glyph info sign ones work a bit differently
     $(function () { $('.glyphicon-info-sign[data-toggle="popover"]').popover(
         {
-            delay: {
-                'hide': 500
-            },
             html: true,
             placement: 'bottom',
-            trigger: 'manual'
+            trigger: 'click'
         }
-    ).on('mouseenter', function () {
-        var _this = this;
-        $(this).popover('show');
-        $(".popover").on('mouseleave', function () {
-            $(_this).popover('hide');
-        });
-    }).on('mouseleave', function () {
-        var _this = this;
-        setTimeout(function () {
-            if (!$('.popover:hover').length) {
-                $(_this).popover('hide');
+    )});
+
+    // Filter websites shown based on filter input
+    $('#filter-sites').on('keyup', function (e) {
+        var search = e.target.value;
+        // Clear previous search
+        $('table, tr').show();
+
+        $('tr td:first-of-type a:first-of-type').each(function (i, link) {
+            var linkName = link.textContent;
+            if (linkName.match(search)) {
+                return;
             }
-        }, 500);
+            $(link).closest('tr').hide();
+            return;
+        });
+
+        $('tbody').each(function (i, tbody) {
+            if ($(tbody).children(':visible').length === 0) {
+                $(tbody).closest('table').hide();
+            }
         });
     });
 });
